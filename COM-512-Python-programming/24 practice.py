d21bb4441e576468a1b3ce12285238e15f3b5867
Safal <safalpandita2006@gmail.com>
2026-09-10 02:36:21 +0530

    Safal

---
+++ COM-512-Python-programming/24 practice.py
@@ -0,0 +1,8 @@
+# Write a python program to detect double space in a string
+
+# Write a python program to format the following letter using escape sequence characters.
+# letter = "Dear Saurabh, this python course is nice. Thanks!"
+
+text = input("enter a string: ")
+print(text.find(" "))
+
