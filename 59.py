6f7ae64f334e3b88ec802a7c85c6309774fd54cd
rujulgupta <rujulgupta7@gmail.com>
2026-09-15 12:00:16 +0530

    15th sept files

---
+++ 59.py
@@ -0,0 +1,13 @@
+# Write a Python program to repeatedly calculate the sum of digits of a number until the result becomes a single digit.
+n = int(input("Enter a number: "))
+
+while n >= 10:
+    sum = 0
+
+    while n > 0:
+        sum = sum + n % 10
+        n = n // 10
+
+    n = sum
+
+print("Single digit:", n)
